b10422f3188d91398a463120f5d4dc90753a27ae
rrasp94 <ruraspud@vsite.hr>
2025-05-21 14:53:42 +0200

    Implement CreateShip method in Fleet class.

--- Model/Fleet.cs
+++ Model/Fleet.cs
@@ -4,4 +4,9 @@ public class Fleet
 {
     private List<Ship> ships = new List<Ship>();
     public IEnumerable<Ship> Ships { get { return ships; } }
+    public void CreateShip(List<Square> squares)
+    {
+        var ship = new Ship(squares);
+        ships.Add(ship);
+    }
 }
