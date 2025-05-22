bd8f2514f0d19c57306f278da94dcd309777ac51
rrasp94 <ruraspud@vsite.hr>
2025-05-22 11:40:07 +0200

    Create tests for Hit in Fleet, Sart Gunnery implementation, Implement shooting tactics change, Implement Hit method for Fleet

---
+++ Model/Gunnery.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+
+namespace Model;
+
+public enum ShootingTactics
+{
+    Random,
+    Surrounding,
+    Inline
+}
+
+public class Gunnery
+{
+    public Gunnery(int rows, int columns, IEnumerable<int> shipLengths)
+    {
+        recordGrid = new Grid(rows, columns);
+    }
+
+    public SquareCoordinate Next()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ProcessHitResult(HitResult hitResult)
+    {
+        switch (hitResult)
+        {
+            case HitResult.Missed:
+                return;
+            case HitResult.Hit:
+                switch (ShootingTactics)
+                {
+                    case ShootingTactics.Random:
+                        ChangeTacticsToSurrounding();
+                        return;
+                    case ShootingTactics.Surrounding:
+                        ChangeTacticsToInline(); break;
+                    case ShootingTactics.Inline:
+                        return;
+                    default:
+                        Debug.Assert(false);
+                        return;
+                }
+                return;
+            case HitResult.Sunken:
+                ChangeTacticsToRandom();
+                return;
+        }
+    }
+
+    private void ChangeTacticsToRandom()
+    {
+        ShootingTactics = ShootingTactics.Random;
+    }
+
+    private void ChangeTacticsToSurrounding()
+    {
+        ShootingTactics = ShootingTactics.Surrounding;
+    }
+
+    private void ChangeTacticsToInline()
+    {
+        ShootingTactics = ShootingTactics.Inline;
+    }
+
+    public ShootingTactics ShootingTactics { get; private set; } = ShootingTactics.Random;
+    private readonly Grid recordGrid;
+    private ITargetSelector targetSelector = new RandomTargetSelector();
+}
